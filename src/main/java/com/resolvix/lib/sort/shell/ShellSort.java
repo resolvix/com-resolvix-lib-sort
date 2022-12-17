d1df1f887fc359f50e5225ee331167b021968c79
Roger Bisson <rwbisson@yahoo.com>
2022-12-17 17:15:46 +0000

    Refactored ShellSort and UTs to compile with API

--- src/main/java/com/resolvix/lib/sort/shell/ShellSort.java
+++ src/main/java/com/resolvix/lib/sort/shell/ShellSort.java
@@ -1,16 +1,20 @@
 package com.resolvix.lib.sort.shell;
 
+import com.resolvix.lib.sort.api.SortAlgorithm;
+
 import java.util.Comparator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class ShellSort {
+public class ShellSort
+    implements SortAlgorithm
+{
 
-    private static <T> int[] getGapSequence(T[] ts) {
+    private <T> int[] getGapSequence(T[] ts) {
         return new int[] { 5, 2, 1 };
     }
 
-    public static <T> void sort(T[] ts, Comparator<T> c) {
+    public <T> void sort(T[] ts, Comparator<T> c) {
         int[] gapSequence = getGapSequence(ts);
         for (int gap : gapSequence) {
             int i_max = ts.length;
@@ -25,11 +29,7 @@ public class ShellSort {
         }
     }
 
-    public static <T extends Comparable<? super T>> void sort(List<T> list) {
-        throw new UnsupportedOperationException();
-    }
-
-    public static <T> void sort(List<T> list, Comparator<? super T> c) {
+    public <T> void sort(List<T> list, Comparator<? super T> c) {
         @SuppressWarnings("unchecked")
         T[] ts = (T[]) list.toArray();
         sort(ts, c);
@@ -38,5 +38,9 @@ public class ShellSort {
             i.next();
             i.set(t);
         }
+    }
+
+    public <T extends Comparable<? super T>> void sort(List<T> list) {
+        sort(list, T::compareTo);
     }
 }
--- src/test/java/com/resolvix/lib/sort/shell/ShellSortUT.java
+++ src/test/java/com/resolvix/lib/sort/shell/ShellSortUT.java
@@ -3,6 +3,7 @@ package com.resolvix.lib.sort.shell;
 import com.resolvix.lib.sort.bubble.BubbleSort;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -10,28 +11,30 @@ import java.util.List;
 
 public class ShellSortUT {
 
+    ShellSort algorithm = new ShellSort();
+
+    @Before
+    public void before() {
+        //
+    }
+
+    //
+    //  sort array
+    //
+
     @Test
     public void sortIntegerArray() {
         Integer[] ints = new Integer[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-        ShellSort.sort(ints, Integer::compareTo);
+        algorithm.sort(ints, Integer::compareTo);
         Assert.assertThat(
                 ints,
                 Matchers.arrayContaining(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     }
 
     @Test
-    public void sortIntegerList() {
-        List<Integer> ints = Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
-        ShellSort.sort(ints, Integer::compareTo);
-        Assert.assertThat(
-                ints,
-                Matchers.contains(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-    }
-
-    @Test
     public void sortCharacterArray() {
         Character[] chars = new Character[] { 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A' };
-        ShellSort.sort(chars, Character::compareTo);
+        algorithm.sort(chars, Character::compareTo);
         Assert.assertThat(
                 chars,
                 Matchers.arrayContaining('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'));
@@ -42,10 +45,45 @@ public class ShellSortUT {
         String[] strings = new String[] {
                 "one", "two", "three", "four", "five",
                 "six", "seven", "eight", "nine", "ten" };
-        ShellSort.sort(strings, String::compareTo);
+        algorithm.sort(strings, String::compareTo);
         Assert.assertThat(
                 strings,
                 Matchers.arrayContaining(
+                        "eight", "five", "four", "nine", "one",
+                        "seven", "six", "ten", "three", "two"));
+    }
+
+    //
+    //  sort list
+    //
+
+    @Test
+    public void sortIntegerList() {
+        List<Integer> ints = Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+        algorithm.sort(ints);
+        Assert.assertThat(
+                ints,
+                Matchers.contains(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
+    @Test
+    public void sortCharacterList() {
+        List<Character> chars = Arrays.asList('J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A');
+        algorithm.sort(chars);
+        Assert.assertThat(
+                chars,
+                Matchers.contains('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'));
+    }
+
+    @Test
+    public void sortStringList() {
+        List<String> strings = Arrays.asList(
+                "one", "two", "three", "four", "five",
+                "six", "seven", "eight", "nine", "ten");
+        algorithm.sort(strings);
+        Assert.assertThat(
+                strings,
+                Matchers.contains(
                         "eight", "five", "four", "nine", "one",
                         "seven", "six", "ten", "three", "two"));
     }
