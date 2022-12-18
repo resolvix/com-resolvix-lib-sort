e90efa27227417e77b4a17dd2c3f53c970c2cda5
Roger Bisson <rwbisson@resolvix.com>
2022-12-18 23:36:08 +0000

    Added InsertionSort and UTs; updated README.md

--- README.md
+++ README.md
@@ -13,6 +13,15 @@ algorithm is O(n<sup>2</sup>).
 The bubble sort algorithm is more particularly described in the paper: 
 [Bubble Sort: An Archealogical Algorithmic Analysis](https://users.cs.duke.edu/~ola/bubble/bubble.pdf).
 
+### Insertion sort
+
+Provides a basic implementation of the insertion sort algorithm. The worst-case
+and best-case performance of the algorithm are O(n<sup>2</sup>) and O(n)
+respectively.
+
+The insertion sort algorithm is more particularly described in the paper:
+[A high-speed sorting procedure](https://dl.acm.org/doi/10.1145/366947.366957).
+
 ### Shellsort
 
 Provides a basic implementation of the Shellsort, a development of the bubble
---
+++ src/test/java/com/resolvix/lib/sort/insertion/InsertionSortUT.java
@@ -0,0 +1,128 @@
+package com.resolvix.lib.sort.insertion;
+
+import com.resolvix.lib.sort.shell.ShellSort;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+public class InsertionSortUT {
+
+    InsertionSort algorithm = new InsertionSort();
+
+    @Before
+    public void before() {
+        //
+    }
+
+    //
+    //  sort array
+    //
+
+    @Test
+    public void sortIntegerArray() {
+        Integer[] ints = new Integer[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        algorithm.sort(ints, Integer::compareTo);
+        Assert.assertThat(
+            ints,
+            Matchers.arrayContaining(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
+    @Test
+    public void sortIntegerArrayOddNumberOfElements() {
+        Integer[] ints = new Integer[]{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        algorithm.sort(ints, Integer::compareTo);
+        Assert.assertThat(
+            ints,
+            Matchers.arrayContaining(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+    }
+
+    @Test
+    public void sortCharacterArray() {
+        Character[] chars = new Character[]{'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'};
+        algorithm.sort(chars, Character::compareTo);
+        Assert.assertThat(
+            chars,
+            Matchers.arrayContaining('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'));
+    }
+
+    @Test
+    public void sortCharacterArrayOddNumberOfElements() {
+        Character[] chars = new Character[]{
+            'O', 'N', 'M', 'L', 'K',
+            'J', 'I', 'H', 'G', 'F',
+            'E', 'D', 'C', 'B', 'A'};
+        algorithm.sort(chars, Character::compareTo);
+        Assert.assertThat(
+            chars,
+            Matchers.arrayContaining(
+                'A', 'B', 'C', 'D', 'E',
+                'F', 'G', 'H', 'I', 'J',
+                'K', 'L', 'M', 'N', 'O'));
+    }
+
+    @Test
+    public void sortStringArray() {
+        String[] strings = new String[]{
+            "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten"};
+        algorithm.sort(strings, String::compareTo);
+        Assert.assertThat(
+            strings,
+            Matchers.arrayContaining(
+                "eight", "five", "four", "nine", "one",
+                "seven", "six", "ten", "three", "two"));
+    }
+
+    @Test
+    public void sortStringArrayOddNumberOfElements() {
+        String[] strings = new String[]{
+            "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen"};
+        algorithm.sort(strings, String::compareTo);
+        Assert.assertThat(
+            strings,
+            Matchers.arrayContaining(
+                "eight", "eleven", "five", "four", "nine",
+                "one", "seven", "six", "ten", "thirteen",
+                "three", "twelve", "two"));
+    }
+
+    //
+    //  sort list
+    //
+
+    @Test
+    public void sortIntegerList() {
+        List<Integer> ints = Arrays.asList(10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+        algorithm.sort(ints);
+        Assert.assertThat(
+            ints,
+            Matchers.contains(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
+    @Test
+    public void sortCharacterList() {
+        List<Character> chars = Arrays.asList('J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A');
+        algorithm.sort(chars);
+        Assert.assertThat(
+            chars,
+            Matchers.contains('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'));
+    }
+
+    @Test
+    public void sortStringList() {
+        List<String> strings = Arrays.asList(
+            "one", "two", "three", "four", "five",
+            "six", "seven", "eight", "nine", "ten");
+        algorithm.sort(strings);
+        Assert.assertThat(
+            strings,
+            Matchers.contains(
+                "eight", "five", "four", "nine", "one",
+                "seven", "six", "ten", "three", "two"));
+    }
+}
